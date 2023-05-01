05ad62ead98bee60b41dbfd3b2405094f95779ba
CassDabi <38007056+CassDabii@users.noreply.github.com>
2023-05-01 18:12:13 +0100

    Update main.py

--- STOCHAST/main.py
+++ STOCHAST/main.py
@@ -29,42 +29,41 @@ def firstImage():
     one = Image.open('STOCHAST/IMG_1469-4.jpeg')
     with containers.pop(random.randrange(len(containers))):
             col1.image(one, width=200)
-            col2.text("""A twenty euro on the table to cover the meal, then emptied the rest of the contents of the purse on
+            col2.write("""A twenty euro on the table to cover the meal, then emptied the rest of the contents of the purse on
 the table. I had no more need for money and I want them to know.""")
 
 
 def secondImage():
     two = Image.open('STOCHAST/IMG_1944-2.jpeg')
     with containers.pop(random.randrange(len(containers))):
-            st.image(two, width=200)
-            st.write("""So this is revenge?. Im doing this for you. Whatever you may think of me I wouldn’t do something
+            col1.image(two, width=200)
+            col2.write("""So this is revenge?. Im doing this for you. Whatever you may think of me I wouldn’t do something
 that low. I cant trust them.""")
 
 
 def thirdImage():
     three = Image.open('STOCHAST/IMG_1990-2.jpeg')
     with containers.pop(random.randrange(len(containers))):
-            st.image(three, width=200)
-            st.write("""The eating grew to an end. He lowered his voice as though they could be heard over the din and
+            col1.image(three, width=200)
+            col2.write("""The eating grew to an end. He lowered his voice as though they could be heard over the din and
 clatter. Ive been watchin him you know. I invite you to join me in the next room for entertainment.""")
 
 
 def fourthImage():
     four = Image.open('STOCHAST/IMG_2061.jpeg')
     with containers.pop(random.randrange(len(containers))):
-            st.image(four, width=200)
-            st.write("""Say Bermondsey and they wrinkle their noses. Still, it was the home before all homes. The river
+            col1.image(four, width=200)
+            col2.write("""Say Bermondsey and they wrinkle their noses. Still, it was the home before all homes. The river
 lapped beneath us as we slept. Dark water heaved up an odd sullen grey.""")
 
 
 def fiveImage():
     five = Image.open('STOCHAST/IMG_2191.jpeg')
     with containers.pop(random.randrange(len(containers))):
-            st.image(five, width=200)
-            st.write("""Im all out of pop groups, was I wrong to be angry? You’ve done everything u could. I sighed heavily. I
+            col1.image(five, width=200)
+            col2.write("""Im all out of pop groups, was I wrong to be angry? You’ve done everything u could. I sighed heavily. I
 just wanted to…. What was I thinking?.""")
-            st.divider()
-        
+      
 
 
 function_list = [firstImage ,secondImage ,thirdImage, fourthImage, fiveImage]
